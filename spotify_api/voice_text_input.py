0b1d2d5d9d548b776438778b835d89a76f0b61cb
Alex Ellison <alexe@DESKTOP-0U8EAR3.mit.edu>
2021-05-03 00:25:21 -0400

    untested database stuff and audio analysis basics

--- spotify_api/voice_text_input.py
+++ spotify_api/voice_text_input.py
@@ -4,7 +4,12 @@ from dotenv import load_dotenv
 import json
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import sqlite3
+from sqlite3 import Error
+import datetime
 
+server_user = 'team15'
+ht_db = f'/var/jail/home/{server_user}/final/songs.db'
 load_dotenv()
 HEADERS = {
     'Authorization': 'Bearer {token}'.format(token=os.environ.get("ACCESS_TOKEN"))
@@ -21,20 +26,22 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=os.environ
 def request_handler(request):
     if request['method'] =="GET":
         username = request["values"]["user"]
+        group_name = request["values"]["group"]
         voice_input = request["values"]["voice"]
         command, data = parse_voice_input(voice_input)
         response = None
         if command == "play" and data.get("song_name"):
             response = get_song_uri(data.get("song_name"))
-            print(response)
+            add_song_to_db(song_uri=response.get("track_uri"), song_name=data.get("song_name"), group_name=group_name)
+            # play_song(response['track_uri'])
         elif command == "add" and data.get("song_name"):
-            # TODO: add song to the queue instead of just playing
-            pass
-        elif command == "pause":
-            # Todo: pause playback
-            pass
-        return response
+            response = get_song_uri(data.get("song_name"))
+            add_song_to_db(song_uri=response.get("track_uri"), song_name=data.get("song_name"), group_name=group_name)
 
+            # add_song_to_queue(response['track_uri'])
+        elif command == "pause":
+            pause()
+        return response
     else:
         return "invalid HTTP method for this url."
 
@@ -59,6 +66,19 @@ def request_handler(request):
 #
 #     return access_token
 
+def create_db():
+    with sqlite3.connect(ht_db) as c:
+        c.execute("""CREATE TABLE IF NOT EXISTS queue (time_ timestamp, group_name text, song_name text, song_uri text, 
+        tempo real, energy real, time_signature integer, danceability real, segments text);""")
+
+
+def add_song_to_db(song_uri, song_name, group_name):
+    tempo, energy, time_signature, danceability, segments = get_audio_features(song_uri)
+    now = datetime.datetime.now()
+    with sqlite3.connect(ht_db) as c:
+        c.execute("""INSERT into queue VALUES (?,?,?,?,?,?,?,?,?)""", (now, group_name, song_name, tempo, energy,
+                                                                             time_signature, danceability,
+                                                                             json.dumps(segments)))
 
 def clean_input(voice_input):
     voice_input = voice_input.replace("to the queue", "")
@@ -141,31 +161,35 @@ def add_song_to_queue(song_uri):
 def pause():
     sp.pause_playback()
 
-def get_song_analysis(song_uri):
-    res = sp.audio_analysis(track_id=song_uri)
-    print(res)
+def get_audio_features(song_uri):
+    res = sp.audio_features(tracks=[song_uri])
+    tempo, danceability = res[0].get('tempo'), res[0].get('danceability')
+    energy, time_signature = res[0].get('energy'), res[0].get('time_signature')
+    res = sp.audio_analysis(song_uri)
+    segments = [{'start': x.get('start'), 'duration': x.get('duration'), 'loudness': x.get('loudness')} for x in res.get('sections')]
+    return tempo, energy, time_signature, danceability, segments
 
-
-def generate_token():
-    url = "https://accounts.spotify.com/authorize"
-    data = json.dumps({
-        "client_id": os.environ.get("SPOTIFY_CLIENT_ID"),
-        "response_type": "code",
-        "redirect_uri": "https://example.com/callback",
-        "scope": "user-read-private"
-    })
-    print(data)
-    res = requests.get(url, params=data)
-    print(res.url)
-    print(res.status_code)
-    print(res.text)
+#
+# def generate_token():
+#     url = "https://accounts.spotify.com/authorize"
+#     data = json.dumps({
+#         "client_id": os.environ.get("SPOTIFY_CLIENT_ID"),
+#         "response_type": "code",
+#         "redirect_uri": "https://example.com/callback",
+#         "scope": "user-read-private"
+#     })
+#     print(data)
+#     res = requests.get(url, params=data)
+#     print(res.url)
+#     print(res.status_code)
+#     print(res.text)
 
 if __name__ == "__main__":
-    # print(get_spotify_token())
     # req = {
     #     "method": "GET",
     #     "values": {
     #         "user": "acelli",
+    #         "group": "group15",
     #         "voice": "Play despacito"
     #     }
     # }
@@ -174,9 +198,19 @@ if __name__ == "__main__":
     #     "method": "GET",
     #     "values": {
     #         "user": "acelli",
-    #         "voice": "can you play despacito by Luis Fonzi"
+    #         "group": "group15",
+    #         "voice": "add sunburn to the queue"
     #     }
     # }
     # request_handler(req2)
-    play_song("spotify:track:6habFhsOp2NvshLv26DqMb")
-# print(send_song_name(parse_voice_input("play Despacito")))
+    # import time
+    # time.sleep(10)
+    # req3 = {
+    #     "method": "GET",
+    #     "values": {
+    #         "user": "acelli",
+    #         "voice": "pause"
+    #     }
+    # }
+    # request_handler(req3)
+    get_audio_features('spotify:track:6habFhsOp2NvshLv26DqMb')
