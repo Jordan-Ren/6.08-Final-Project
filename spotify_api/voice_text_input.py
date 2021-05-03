b64a92d11da55448debe6619e96dd9b0070e13a8
Alex Ellison <alexe@DESKTOP-0U8EAR3.mit.edu>
2021-05-03 09:49:57 -0400

    cleanup

--- spotify_api/voice_text_input.py
+++ spotify_api/voice_text_input.py
@@ -5,7 +5,6 @@ import json
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import sqlite3
-from sqlite3 import Error
 import datetime
 
 server_user = 'team15'
@@ -37,7 +36,6 @@ def request_handler(request):
         elif command == "add" and data.get("song_name"):
             response = get_song_uri(data.get("song_name"))
             add_song_to_db(song_uri=response.get("track_uri"), song_name=data.get("song_name"), group_name=group_name)
-
             # add_song_to_queue(response['track_uri'])
         elif command == "pause":
             pause()
@@ -45,26 +43,6 @@ def request_handler(request):
     else:
         return "invalid HTTP method for this url."
 
-# def get_spotify_token():
-#     CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
-#     CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
-#     if CLIENT_ID == None: CLIENT_ID = os.environ.get('CLIENT_ID')
-#     if CLIENT_SECRET == None: CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
-#     AUTH_URL = 'https://accounts.spotify.com/api/token'
-#
-#     # POST
-#     auth_response = requests.post(AUTH_URL, {
-#         'grant_type': 'client_credentials',
-#         'client_id': CLIENT_ID,
-#         'client_secret': CLIENT_SECRET,
-#     })
-#
-#     # convert the response to JSON
-#     auth_response_data = auth_response.json()
-#     # save the access token
-#     access_token = auth_response_data['access_token']
-#
-#     return access_token
 
 def create_db():
     with sqlite3.connect(ht_db) as c:
@@ -108,11 +86,6 @@ def parse_voice_input(voice_input):
     except Exception as e:
         print(e)
         raise e
-    # parsed_voice = voice_input
-    # if "play" in voice_input:
-    #     parsed_voice = parsed_voice.replace('play', '')
-    # print(parsed_voice)
-    # return parsed_voice.strip()
 
 
 def get_song_uri(song):
@@ -124,35 +97,10 @@ def get_song_uri(song):
         return response_data
     else:
         return "Song not found"
-    # # access_token = get_spotify_token()
-    # url = BASE_URL + f"search?q={song}&type=track&limit=1"
-    # r = requests.get(url, headers=HEADERS)
-    #
-    # # track_id = '6y0igZArWVi6Iz0rj35c1Y'
-    # # r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
-    # r = r.json()
-    # response_data = {}
-    # if len(r["tracks"]["items"]) > 0:
-    #     response_data['track_uri'] = r["tracks"]["items"][0]["uri"]
-    #     response_data['url'] = r["tracks"]["items"][0]["external_urls"]["spotify"]
-    #     return response_data
-    # else:
-    #     return "Song not found"
 
 
 def play_song(song_uri):
     sp.start_playback(uris=[song_uri], position_ms=0)
-    # url = BASE_URL + "me/player/play"
-    # data = json.dumps({
-    #     "uris": [song_uri],
-    #     "position_ms": 0,
-    # })
-    # res = requests.put(url, headers=HEADERS, data=data)
-    # print(url)
-    # print(data)
-    # print(res.status_code)
-    # print(res.text)
-    # return res.status_code
 
 
 def add_song_to_queue(song_uri):
@@ -169,20 +117,6 @@ def get_audio_features(song_uri):
     segments = [{'start': x.get('start'), 'duration': x.get('duration'), 'loudness': x.get('loudness')} for x in res.get('sections')]
     return tempo, energy, time_signature, danceability, segments
 
-#
-# def generate_token():
-#     url = "https://accounts.spotify.com/authorize"
-#     data = json.dumps({
-#         "client_id": os.environ.get("SPOTIFY_CLIENT_ID"),
-#         "response_type": "code",
-#         "redirect_uri": "https://example.com/callback",
-#         "scope": "user-read-private"
-#     })
-#     print(data)
-#     res = requests.get(url, params=data)
-#     print(res.url)
-#     print(res.status_code)
-#     print(res.text)
 
 if __name__ == "__main__":
     # req = {
